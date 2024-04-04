fae12a73ec244a9cfee4b8251bc0298a100d2f89
pCarbonero <pablo.carbonero@iesnervion.es>
2024-04-04 13:15:54 +0200

    [u`d] ej 02 mas o menos

--- Tema10Vistas/BoletinAW.sql
+++ Tema10Vistas/BoletinAW.sql
@@ -12,8 +12,41 @@ INNER JOIN SalesLT.Address A ON CA.AddressID = A.AddressID WHERE A.CountryRegion
 SELECT * FROM SalesLT.ProductCategory
 SELECT Name, MAX(ListPrice) FROM SalesLT.Product GROUP BY Name
 
-SELECT PA.Name, MAX(P.ListPrice) AS MaxPrice, MIN(P.ListPrice) AS MinPrice
+SELECT PA.Name, P.Name, MIN(P.ListPrice) AS MinPrice
 FROM SalesLT.ProductCategory PA
 INNER JOIN SalesLT.Product P ON PA.ProductCategoryID = P.ProductCategoryID
-GROUP BY PA.Name
+Group By PA.Name, P.Name, P.ListPrice
+ORDER BY PA.Name, P.ListPrice
 
+SELECT PA.Name, P.Name, MAX(P.ListPrice) AS MAXPrice
+FROM SalesLT.ProductCategory PA
+INNER JOIN SalesLT.Product P ON PA.ProductCategoryID = P.ProductCategoryID
+Group By PA.Name, P.Name, P.ListPrice
+ORDER BY PA.Name, P.ListPrice
+
+-------------------------------------------------------------------------------------------
+SELECT PA.Name, P.Name, MIN(P.ListPrice) AS MinPrice
+FROM SalesLT.ProductCategory PA
+INNER JOIN SalesLT.Product P ON PA.ProductCategoryID = P.ProductCategoryID
+WHERE P.Name IN
+	(SELECT top 1 SalesLT.Product.Name
+	FROM SalesLT.Product WHERE PA.ProductCategoryID = SalesLT.Product.ProductCategoryID
+	order by SalesLT.Product.ListPrice)
+GROUP BY PA.Name, P.Name, P.ListPrice
+
+
+SELECT PA.Name, P.Name, Max(P.ListPrice) AS MaxPrice
+FROM SalesLT.ProductCategory PA
+INNER JOIN SalesLT.Product P ON PA.ProductCategoryID = P.ProductCategoryID
+WHERE P.Name IN
+	(SELECT top 1 SalesLT.Product.Name
+	FROM SalesLT.Product WHERE PA.ProductCategoryID = SalesLT.Product.ProductCategoryID
+	order by SalesLT.Product.ListPrice desc)
+GROUP BY PA.Name, P.Name, P.ListPrice
+-----------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------
+
+--3. Total de Ventas en cada país en dinero (Ya hecha en el boletín 9.3).
+
+--4. Número de clientes que tenemos en cada país. Contaremos cada dirección como si fuera un cliente distinto.
