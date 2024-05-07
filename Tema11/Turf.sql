68f420c1676fd00f764caf8aeb0395b1e7b8bab3
pCarbonero <pablo.carbonero@iesnervion.es>
2024-05-07 14:07:44 +0200

    [upd] 02 turf

--- Tema11/Turf.sql
+++ Tema11/Turf.sql
@@ -15,5 +15,47 @@ GROUP BY C.ID, C.Nombre, C.Sexo, C.FechaNacimiento)
 
 SELECT * FROM dbo.FnCarrerasCaballo('2018-03-01', '2018-03-10')
 
---2. 2.Crea una función escalar llamada FnTotalApostadoCC que reciba como parámetros el ID de un caballo y
+--2. Crea una función escalar llamada FnTotalApostadoCC que reciba como parámetros el ID de un caballo y
 -- el ID de una carrera y nos devuelva el dinero que se ha apostado a ese caballo en esa carrera.
+
+Create or Alter function FnTotalApostadoCC(@idCaballo int, @idCarrera int)
+returns int
+AS BEGIN
+declare @dinero int = (SELECT Importe from LTApuestas WHERE IDCaballo = @idCaballo AND IDCarrera = @idCarrera)
+return @dinero
+END
+
+SELECT dbo.FnTotalApostadoCC(1,1)
+
+--3. Crea una función escalar llamada FnPremioConseguido que reciba como parámetros el ID de una apuesta 
+-- y nos devuelva el dinero que ha ganado dicha apuesta. Si todavía no se conocen las posiciones de los caballos, 
+-- devolverá un NULL. 
+SELECT * FROM LTApuestas
+SELECT * FROM LTCaballosCarreras
+
+CREATE OR ALTER function FnPremioConseguido(@idApuesta int)
+RETURNS decimal(2,1)
+AS BEGIN
+declare @caballo int
+declare @carrera int
+declare @posicion int
+declare @importe decimal(3,1)
+declare @premio decimal (3,1)
+
+SELECT @caballo = IDcaballo, @carrera = IDCarrera, @importe = Importe FROM LTApuestas WHERE ID = @idApuesta
+SELECT @posicion = Posicion FROM LTCaballosCarreras WHERE @caballo = IDCaballo AND @carrera = IDCarrera
+
+CASE @posicion
+     WHEN 1 THEN SET @premio = 
+END 
+
+
+
+END
+
+SELECT dbo.FnPremioConseguido(1)
+
+
+
+
+
