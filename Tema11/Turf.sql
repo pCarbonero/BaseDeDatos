978fb5fe9f83a82c1dc050f19f58da1cfd51a273
pCarbonero <pablo.carbonero@iesnervion.es>
2024-05-08 13:02:32 +0200

    f[UPD] puta mierada

--- Tema11/Turf.sql
+++ Tema11/Turf.sql
@@ -34,28 +34,69 @@ SELECT * FROM LTApuestas
 SELECT * FROM LTCaballosCarreras
 
 CREATE OR ALTER function FnPremioConseguido(@idApuesta int)
-RETURNS decimal(2,1)
+RETURNS numeric
 AS BEGIN
-declare @caballo int
-declare @carrera int
-declare @posicion int
-declare @importe decimal(3,1)
-declare @premio decimal (3,1)
+declare @caballo numeric
+declare @carrera numeric
+declare @posicion numeric
+declare @importe numeric
+declare @premio numeric
+declare @dineroObtenido numeric
 
 SELECT @caballo = IDcaballo, @carrera = IDCarrera, @importe = Importe FROM LTApuestas WHERE ID = @idApuesta
 SELECT @posicion = Posicion FROM LTCaballosCarreras WHERE @caballo = IDCaballo AND @carrera = IDCarrera
 
-CASE @posicion
-     WHEN 1 THEN SET @premio = 
-END 
+IF (@posicion = 1)
+	begin
+     SET @dineroObtenido = (SELECT Premio1*@importe FROM LTCaballosCarreras WHERE @caballo = IDCaballo AND @carrera = IDCarrera)
+	END 
+ELSE IF (@posicion = 2)
+	BEGIN
+	 SET @dineroObtenido = (SELECT Premio2*@importe FROM LTCaballosCarreras WHERE @caballo = IDCaballo AND @carrera = IDCarrera)
+	END
+ELSE IF (@posicion is NULL)
+	BEGIN
+	SET @dineroObtenido = NULL
+	END
+ELSE
+	BEGIN
+	SET @dineroObtenido = -@importe
+	END
 
+return @dineroObtenido
+END
+	
+SELECT dbo.FnPremioConseguido(2)
 
+--4. 
+SELECT * FROM LTApuestas
+SELECT * FROM LTCaballosCarreras
 
+CREATE OR ALTER FUNCTION FnCaballosCarrera(@idCarrera int)
+RETURNS TABLE
+RETURN(SELECT IDCaballo, SUM((dbo.FnDineroApostado(@idCarrera)/dbo.FnTotalApostadoCC(@idCarrera,IDCaballo))) AS A FROM LTApuestas 
+		WHERE IDCarrera = @idCarrera GROUP BY IDCaballo)
+
+SELECT * FROM dbo.FnCaballosCarrera(1) 
+
+CREATE OR ALTER FUNCTION FnDineroApostado(@idCarrera int)
+RETURNS int
+AS BEGIN
+	declare @totalCarrera int = (SELECT SUM(Importe) FROM LTApuestas WHERE IDCarrera = @idCarrera)
+	return @totalCarrera
 END
 
-SELECT dbo.FnPremioConseguido(1)
+SELECT dbo.FnDineroApostado(1) as TotalCarrera
 
+SELECT dbo.FnTotalApostadoCC(1,1)  as TotalAUnCaballo
 
+CREATE OR ALTER FUNCTION FnPremio1(@idCarrera int, @idCaballo int)
+RETURNS decimal(2,1)
+AS BEGIN
+	declare @Premio1 decimal(2,1) = (dbo.FnDineroApostado(@idCarrera)/dbo.FnTotalApostadoCC(@idCarrera, @idCaballo) * 0.6)
+Return @Premio1
+END
 
+SELECT dbo.FnPremio1(1,1)
 
 
